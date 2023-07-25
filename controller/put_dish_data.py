2bd74e5ec53fb0d28243c64987be4988fbe02476
RaenonX <raenonx0710@gmail.com>
2023-07-24 22:41:34 -0500

    Added dish data exporter

---
+++ controller/put_dish_data.py
@@ -0,0 +1,31 @@
+import json
+
+import pymongo
+from pymongo import MongoClient
+
+CONNECTION_STRING = "mongodb://localhost:23015"
+
+
+with open("../data/dish_data.json") as f:
+    data = json.load(f)
+
+
+def main():
+    client = MongoClient(CONNECTION_STRING)
+    db = client.get_database("food")
+    col_info = db.get_collection("dish")
+
+    col_info.drop()
+    col_info.create_index(
+        [("id", pymongo.ASCENDING)],
+        unique=True
+    )
+    col_info.create_index(
+        [("ingredients.id", pymongo.ASCENDING)],
+    )
+
+    col_info.insert_many(data)
+
+
+if __name__ == '__main__':
+    main()
