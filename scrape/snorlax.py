3f6da2550e2634b75db19db7967384d870faca25
RaenonX <raenonx0710@gmail.com>
2023-08-01 18:31:11 -0500

    Added map 3 handling for Snorlax data

--- scrape/snorlax.py
+++ scrape/snorlax.py
@@ -7,7 +7,7 @@ PAGE_URL = "https://serebii.net/pokemonsleep/snorlaxratings.shtml"
 
 
 def main():
-    snorlax_rank_data = {1: [], 2: []}
+    snorlax_rank_data = {1: [], 2: [], 3: []}
     rewards = []
 
     req = send_requests([PAGE_URL])[0]
@@ -17,7 +17,7 @@ def main():
         if idx < 2:
             continue
 
-        rank, energy_1, _, energy_2, __, shards = index_row.find_all("td")
+        rank, energy_1, _, energy_2, __, energy_3, __, shards = index_row.find_all("td")
         _rank_title, _rank_number = rank.text.split(" ")
         rank = {
             "title": MAP_TITLE_TO_ID[_rank_title],
@@ -25,10 +25,12 @@ def main():
         }
         energy_1 = int(energy_1.text.replace(",", ""))
         energy_2 = int(energy_2.text.replace(",", ""))
+        energy_3 = int(energy_3.text.replace(",", ""))
         shards = int(shards.text.split(" ")[0].replace(",", ""))
 
         snorlax_rank_data[1].append({"rank": rank, "energy": energy_1})
         snorlax_rank_data[2].append({"rank": rank, "energy": energy_2})
+        snorlax_rank_data[3].append({"rank": rank, "energy": energy_3})
         rewards.append({"rank": rank, "shard": shards})
 
     to_json(
