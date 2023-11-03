aa98a5acf18deb26c0cb356e0e04cbe935e55710
RaenonX <raenonx0710@gmail.com>
2023-11-02 19:24:27 -0500

    Added RP model run on Azure CI #5

--- calc/_const.py
+++ calc/_const.py
@@ -1,6 +1,7 @@
 # This is the original sheet owned by @goobliss
-# rp_file_id = "1kBrPl0pdAO8gjOf_NrTgAPseFtqQA27fdfEbMBBeAhs"
-RP_MODEL_FILE_ID = "10ojuEmR663BXrDvq0izntYhhe0whylbx9HVKjCayQVk"
+RP_MODEL_FILE_ID = "1kBrPl0pdAO8gjOf_NrTgAPseFtqQA27fdfEbMBBeAhs"
+# This is the cloned sheet for testing
+# RP_MODEL_FILE_ID = "10ojuEmR663BXrDvq0izntYhhe0whylbx9HVKjCayQVk"
 
 RP_MODEL_SHEET_IDS = {
     "data_1_9": "1682088244",
---
+++ run_rp_model.py
@@ -0,0 +1,10 @@
+from calc.rp_model import get_rp_fit_result_df
+
+
+def main():
+    # Running this function generates model pickle file to be used on the server
+    get_rp_fit_result_df()
+
+
+if __name__ == "__main__":
+    main()
