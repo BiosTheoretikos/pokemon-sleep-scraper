26d8b767a079cfd6c3a76d47e398fc4348efe45d
RaenonX <raenonx0710@gmail.com>
2023-11-21 13:03:50 -0600

    Updated submodule dependencies

--- calc/entry.py
+++ calc/entry.py
@@ -2,4 +2,4 @@ from calc.rp_model import get_rp_model_result
 
 
 def get_rp_model_df():
-    return get_rp_model_result("results/{hash_id}.pickle")
+    return get_rp_model_result("results/least-squares-fit.pickle")
