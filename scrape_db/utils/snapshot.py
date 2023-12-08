5c53afcaef21ffbc50cee1591e91469c2d57ca10
RaenonX <raenonx0710@gmail.com>
2023-12-08 07:04:53 -0600

    Added `numpy.int64` JSON serialization handling

--- scrape_db/utils/snapshot.py
+++ scrape_db/utils/snapshot.py
@@ -3,6 +3,8 @@ import logging
 import os
 from typing import Any, Iterable, Mapping
 
+import numpy as np
+
 from scrape_db.utils.env import Environment
 
 
@@ -10,6 +12,8 @@ class JsonEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, set):
             return sorted(obj)
+        if isinstance(obj, np.int64):
+            return int(obj)
 
         return json.JSONEncoder.default(self, obj)
 
