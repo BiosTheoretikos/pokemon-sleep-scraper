3a6c8959244936f0a5fafebd9d4440b9b359b16b
RaenonX <raenonx0710@gmail.com>
2023-12-08 00:04:26 -0600

    Added db export module - ingredient #9

--- scrape_db/main.py
+++ scrape_db/main.py
@@ -1,7 +1,9 @@
 from scrape_db.module.berry import export_berry
+from scrape_db.module.ingredient import export_ingredient
 from scrape_db.utils.log import init_logging
 
 
 def scrape():
     init_logging()
     export_berry()
+    export_ingredient()
---
+++ scrape_db/module/ingredient.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import pymongo
+
+from scrape_db.utils.db import export_to_mongo, open_sql_connection
+from scrape_db.utils.module import start_export_module
+
+
+def export_ingredient():
+    with start_export_module("Ingredient"), open_sql_connection() as connection:
+        df = pd.read_sql("SELECT * FROM cooking_foods", connection)
+
+        data = (
+            df[(df["need_cooking"] == 0) & (df["energy"] > 0)][["id", "energy", "sell_coin"]]
+            .rename(columns={"sell_coin": "price"})
+            .to_dict(orient="records")
+        )
+
+        with export_to_mongo("food", "ingredient", data) as col:
+            col.create_index(
+                [("id", pymongo.ASCENDING)],
+                unique=True
+            )
