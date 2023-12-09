1c319e5765e981fd91e5aefcca7314d718864761
RaenonX <raenonx0710@gmail.com>
2023-12-09 02:49:56 -0600

    Updated `scrape.module.pokemon` to have an main exporting call

--- scrape/main.py
+++ scrape/main.py
@@ -1,8 +1,7 @@
 from scrape.module.berry import export_berry
 from scrape.module.ingredient import export_ingredient
 from scrape.module.map import export_map_meta
-from scrape.module.pokemon.branch import export_pokemon_branch
-from scrape.module.pokemon.main import export_pokemon_info
+from scrape.module.pokemon.main import export_pokemon
 from scrape.module.snorlax import export_snorlax
 from scrape.module.subskill import export_subskill
 from scrape.module.xp_shard import export_xp_shard
@@ -13,8 +12,7 @@ from scrape.utils.log import init_logging
 def scrape():
     init_logging()
 
-    export_pokemon_info()
-    export_pokemon_branch()
+    export_pokemon()
 
     export_berry()
     export_ingredient()
