6d926ef87a56eebdb0992483dcbfd81edc5feb8d
RaenonX <raenonx0710@gmail.com>
2023-12-08 20:55:24 -0600

    Added `numpy.int64` MongoDB codec

--- scrape_db/utils/db/mongo.py
+++ scrape_db/utils/db/mongo.py
@@ -2,6 +2,8 @@ import logging
 from contextlib import contextmanager
 from typing import Any, ContextManager, Iterable, Mapping
 
+import numpy
+from bson.codec_options import CodecOptions, TypeCodec, TypeRegistry
 from pymongo import MongoClient
 from pymongo.collection import Collection
 
@@ -9,6 +11,20 @@ from scrape_db.utils.env import Environment
 from scrape_db.utils.snapshot import take_export_snapshot
 
 _mongo = MongoClient(Environment.mongo_connection)
+
+
+class NumpyInt64Codec(TypeCodec):
+    python_type = numpy.int64
+    bson_type = int
+
+    def transform_python(self, value: numpy.int64):
+        return int(value)
+
+    def transform_bson(self, value: int):
+        return numpy.int64(value)
+
+
+_mongo_codec_options = CodecOptions(type_registry=TypeRegistry([NumpyInt64Codec()]))
 
 
 @contextmanager
@@ -19,7 +35,7 @@ def export_to_mongo(
 ) -> ContextManager[Collection[Mapping[str, Any]]]:
     logging.info("Exporting data to MongoDB collection `%s.%s`", db_name, collection_name)
 
-    collection = _mongo.get_database(db_name).get_collection(collection_name)
+    collection = _mongo.get_database(db_name).get_collection(collection_name, codec_options=_mongo_codec_options)
     yield collection
 
     take_export_snapshot(f"{db_name}.{collection_name.replace('/', '-')}", data)
